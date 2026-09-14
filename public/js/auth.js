193faf645c74db712a92ef26bd818094f1c76e25
portfoliowebs6-collab <portfoliowebs6@gmail.com>
2026-09-14 08:40:39 +0530

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const password = document.getElementById('login-password').value;
 
             try {
-                const res = await fetch('http://localhost:5000/api/auth/login', {
+                const res = await fetch('https://deepak-portfolio-3pkq.onrender.com/api/auth/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email, password })
